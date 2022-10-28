d46cf13ee8066e9bf1108aca5cddd6f1e78592eb
Lopez-CL <cllopez511@gmail.com>
2022-10-27 20:10:19 -0400

    fixed the key age in first component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import PersonCard from './components/PersonCard';
 const App = () => {
   return (
     <div className='App'>
-      <PersonCard fName={ "Janice" } lName={ "Doe" } lName={ 32 } hairColor={ "Black" } />
+      <PersonCard fName={ "Janice" } lName={ "Doe" } age={ 32 } hairColor={ "Black" } />
       <PersonCard fName={ "Smith" } lName={ "John" } age={ 88 } hairColor={ "None" } />
       <PersonCard fName={ "Fillmore" } lName={ "Millard" } age={ 50 } hairColor={ "Brown" } />
       <PersonCard fName={ "Smith" } lName={ "Maria" } age={ 62 } hairColor={ "Brown" } />
